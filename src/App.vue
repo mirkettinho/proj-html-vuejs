32ff7890ca058118da720ae5d64e2f612acf5b77
mirkettinho <mirkodifranco30@gmail.com>
2023-05-09 12:13:21 +0200

    [add] components Header and Footer empty

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<script>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<template>
+
+  <Header />
+  <Footer />
+
+
+</template>
+
+<!-- SCSS -->
+<style lang="scss">
+
+// STILI GENERALI
+@use "./scss/main.scss";
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+
+<script>
+
+
+export default {
+  name: "Header",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
