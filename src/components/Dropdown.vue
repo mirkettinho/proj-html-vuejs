be7e41046ce6df7bde446477c958b3d3ce0d239f
mirkettinho <mirkodifranco30@gmail.com>
2023-05-09 21:41:19 +0200

    [add] pre-made animation

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -10,18 +10,49 @@ export default {
     return{
 
     }
+  }, 
+  methods: {
+      enter(el) {
+        el.style.height = 'auto';
+        const height = getComputedStyle(el).height;
+        el.style.height = 0;
+        getComputedStyle(el);
+
+        setTimeout(() => {
+          el.style.height = height;
+        });        
+      },
+      afterEnter(el) {
+        el.style.height = 'auto';
+      },
+      leave(el) {
+        el.style.height = getComputedStyle(el).height;
+
+        getComputedStyle(el);
+
+        setTimeout(() => {
+          el.style.height = 0;
+        });
+      }
+    }
   }
-}
+
 </script>
 
 <template>
-  <ul v-show="list.open" id="sub-items">
-      <li class="sub-item" v-for="(link,index) in list.subnav" :key="index">
-        <a  @click="link.open = !link.open" :href="link.href">{{ link.title }}</a>
-        <i v-if="link.subnav" :class="{'fa-solid fa-chevron-down' : !link.open, 'fa-solid fa-chevron-up' : link.open}"></i>
-        <Dropdown v-if="link.subnav" :list="link"/>
-      </li>
-    </ul>
+  <transition 
+    name="expand"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @leave="leave">
+    <ul v-show="list.open" id="sub-items">
+        <li class="sub-item" v-for="(link,index) in list.subnav" :key="index">
+          <a  @click="link.open = !link.open" :href="link.href">{{ link.title }}</a>
+          <i v-if="link.subnav" :class="{'fa-solid fa-chevron-down' : !link.open, 'fa-solid fa-chevron-up' : link.open}"></i>
+          <Dropdown v-if="link.subnav" :list="link"/>
+        </li>
+      </ul>
+    </transition>
 </template>
 
 <style lang="scss" scoped>
@@ -29,10 +60,6 @@ export default {
   list-style: none;
   position: absolute;
   top: 120px;
-}
-ul{
-  position: relative;
-  top: 50px;
 }li{
   background-color: white;
   padding: 10px;
@@ -49,4 +76,8 @@ ul{
       }
       
     }
+    .expand-enter-active, .expand-leave-active {
+    transition: height .5s ease-in-out;
+    overflow: hidden;
+  }
 </style>
